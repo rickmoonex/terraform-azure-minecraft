9b98697abee02398ad2f4ef63a8669cbaff95f76
Rick Moonen <rick@moonen.network>
2022-07-28 23:28:20 +0200

    fix: set correct ip address type
    set the correct ip address type for the container instance

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_container_group" "containers" {
   name                = "minecraft"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type     = "public"
+  ip_address_type     = "Public"
   dns_name_label      = "moonenminecraftprod"
   os_type             = "Linux"
 
