d0c0a5a71763c655b7fc4374c3c30f7bcfd0f840
Rick Moonen <rick@moonen.network>
2022-07-28 23:15:18 +0200

    feat: add container storage
    setup the needed resources to supply storage to the container

--- main.tf
+++ main.tf
@@ -2,3 +2,27 @@ resource "azurerm_resource_group" "rg" {
   name     = "minecraft-prod-rg"
   location = "westeurope"
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "moonenminecraftprod"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  access_tier              = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "prod"
+  }
+}
+
+resource "azurerm_storage_share" "world_share" {
+  name                 = "world"
+  storage_account_name = azurerm_storage_account.storage.name
+  quota                = 50
+}
+
+resource "azurerm_storage_share" "config_share" {
+  name                 = "config"
+  storage_account_name = azurerm_storage_account.storage.name
+  quota                = 1
+}
