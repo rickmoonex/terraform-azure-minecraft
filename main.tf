56b8a5e52c7803765a5c4c2ef0c39b64c455d061
Rick Moonen <rick@moonen.network>
2022-07-28 23:27:00 +0200

    fix: set account tier for storage account
    set an account tier for the storage account

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "storage" {
   name                     = "moonenminecraftprod"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  access_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "GRS"
 
   tags = {
